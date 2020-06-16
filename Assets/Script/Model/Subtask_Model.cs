ce5318f9e9d6f2f9ec5810e3a9502d0bc465af94
BrunoGot <bruno.got@orange.fr>
2020-06-16 14:33:16 +0200

    work on GUI for modelisation task. Start to implement subtasks class

---
+++ Assets/Script/Model/Subtask_Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subtask_Model
+{
+    private string m_path;
+    private string m_name;
+    private string m_software;
+    private int m_version;
+    private string m_commentaire;
+    private string m_todo;
+    private string m_retakes;
+
+
+
+}
